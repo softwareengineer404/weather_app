111e5457c49516bfe0f7eac6599c3c17ca2e867c
tesfay <tesfayhailemariam29@gmail.com>
2026-03-27 11:32:56 +0300

    fetch data

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const apiKey = "";
-const apiUrl = "";
+const apiKey = "56e3d4bc5e660913002690c1fb439f89";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=bangalore";
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+}
