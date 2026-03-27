267df8210d5e48fd5addc888405d6ae1c4f22eef
tesfay <tesfayhailemariam29@gmail.com>
2026-03-27 17:35:36 +0300

    check search value

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const apiKey = "56e3d4bc5e660913002690c1fb439f89";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=bangalore";
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
 }
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
  checkWeather();
